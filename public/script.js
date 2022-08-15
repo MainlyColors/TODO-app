8c19e9b6f860ac4d39ac9ee8495dc80d1be1c533
Ryan <ryanmahara@gmail.com>
2022-08-15 11:34:15 -0500

    add mark and unmark functionality to todo list

--- public/script.js
+++ public/script.js
@@ -1,7 +1,23 @@
 const formEl = document.querySelector('form');
+const liNodeList = document.querySelectorAll('li');
 
 formEl.addEventListener('submit', (e) => {
   console.dir(e.target);
   console.log('form submitted');
   console.log(location.href);
 });
+
+Array.from(liNodeList).map((el) => el.addEventListener('click', mark));
+
+function mark(e) {
+  // cross out => a normal item
+  if (e.target.id.includes('completed--js')) {
+    e.target.id = '';
+    e.target.classList.toggle('completed');
+  }
+  //a normal item => cross out
+  else {
+    e.target.id = 'completed--js';
+    e.target.classList.toggle('completed');
+  }
+}
